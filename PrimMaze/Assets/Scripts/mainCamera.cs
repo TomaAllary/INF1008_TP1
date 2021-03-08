61f7f9d91b534cdf5186ea7b9be18cdfec82f372
matteabe <ben.matteau@gmail.com>
2021-03-07 19:39:46 -0500

    not much more
    Minimap lighting is HAAAAARRRDDDD :'( (if you want the player to be in the dark)
    Directions don't work properly
    Still haven't done animations

---
+++ PrimMaze/Assets/Scripts/mainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCamera : MonoBehaviour
+{
+    public Light mainLight;
+
+
+    private void OnPreCull()
+    {
+        mainLight.enabled = true;
+    }
+
+    private void onPostRendre()
+    {
+        mainLight.enabled = false;
+    }
+}
