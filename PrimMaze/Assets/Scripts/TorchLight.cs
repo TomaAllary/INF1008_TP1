6e6adf1f7b8c64c89b634344520412fe15b29946
matteabe <ben.matteau@gmail.com>
2021-03-09 20:28:50 -0500

    Added a script for minimalist torchlight
    Not sure if we are gonna use it but I was curious ^^

---
+++ PrimMaze/Assets/Scripts/TorchLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchLight : MonoBehaviour
+{
+    
+    int torchTick = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (torchTick == 15)
+        {
+            this.GetComponentInChildren<Light>().intensity = Random.Range(0.5f, 2);
+            torchTick = 0;
+        }
+        else
+            torchTick++;
+    }
+
+
+}
